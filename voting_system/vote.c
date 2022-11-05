1451b6417c55e56a713999316da7e713a4f53953
parveenxkashyap <inparv@gmail.com>
2022-11-05 19:25:35 +0530

    add candidate

--- voting_system/vote.c
+++ voting_system/vote.c
@@ -13,3 +13,40 @@ Candidate allCandidates[MAX_C];
 int candidateCount = 0;
 char symbols[10] = { '!', '@', '#', '$', '%', '^', '&', '*', '~', '+' };
 int symbolTaken[11];
+
+void fillCandidate(int cNum)
+{
+    printf("Symbols:\n");
+    for (int j = 0; j < 10; j++) {
+        if (symbolTaken[j] == 1) continue;
+        printf("%d %c\n", j + 1, symbols[j]);
+    }
+
+    int num;
+    printf("Choose symbol for candidate %d: ", cNum + 1);
+    scanf("%d", &num);
+
+    if (num <= 0 || num > 10 || symbolTaken[num - 1] == 1) {
+        printf("Not available, pick again\n");
+        fillCandidate(cNum);
+    } else {
+        symbolTaken[num - 1] = 1;
+        allCandidates[cNum].symbol = symbols[num - 1];
+        printf("Enter name of candidate %d: ", cNum + 1);
+        scanf("%s", allCandidates[cNum].name);
+        allCandidates[cNum].votes = 0;
+    }
+}
+
+void displayAllCandidates()
+{
+    for (int j = 0; j < candidateCount; j++) {
+        printf("%s\t", allCandidates[j].name);
+    }
+    printf("\n");
+    for (int j = 0; j < candidateCount; j++) {
+        printf("%c\t\t", allCandidates[j].symbol);
+    }
+    printf("\n");
+}
+
