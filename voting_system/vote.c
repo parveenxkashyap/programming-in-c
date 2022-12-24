90db1f5372920d9bf54d0ab1f91c0a3101b05daa
parveenxkashyap <inparv@gmail.com>
2022-12-25 05:29:17 +0530

    voting working

--- voting_system/vote.c
+++ voting_system/vote.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+// headers
 
 #define MAX_C 11
+// max candidates
 
 typedef struct Candidate {
     char name[50];
@@ -10,19 +12,66 @@ typedef struct Candidate {
 } Candidate;
 
 Candidate allCandidates[MAX_C];
+
 int candidateCount = 0;
+// possible symbols
 char symbols[10] = { '!', '@', '#', '$', '%', '^', '&', '*', '~', '+' };
+// track which symbol used
 int symbolTaken[11];
 
+// function stuff
+void fillCandidate(int);
+void displayAllCandidates();
+void getVotes(int);
+void getResults();
+
+int main()
+{
+    // init symbolTaken
+    for (int i = 0; i < 11; i++) {
+        symbolTaken[i] = 0;
+    }
+
+    // get number of candidates
+    printf("Enter the number of candidates: ");
+    scanf("%d", &candidateCount);
+    if (candidateCount >= MAX_C) {
+        printf("Too many candidates, bye\n");
+        return 0;
+    }
+
+    // fill candidate info
+    for (int i = 0; i < candidateCount; i++) {
+        fillCandidate(i);
+    }
+
+    // how many voters
+    int numVoters;
+    printf("Enter number of voters: ");
+    scanf("%d", &numVoters);
+
+    // get votes
+    for (int i = 0; i < numVoters; i++) {
+        getVotes(i);
+    }
+
+    // show results
+    getResults();
+
+    return 0;
+}
+
+// add candidate info
 void fillCandidate(int cNum)
 {
     printf("Symbols:\n");
     for (int j = 0; j < 10; j++) {
-        if (symbolTaken[j] == 1) continue;
+        if (symbolTaken[j] == 1)
+            continue;
         printf("%d %c\n", j + 1, symbols[j]);
     }
 
-    int num;
+    int num = 0;
     printf("Choose symbol for candidate %d: ", cNum + 1);
     scanf("%d", &num);
 
@@ -32,12 +81,13 @@ void fillCandidate(int cNum)
     } else {
         symbolTaken[num - 1] = 1;
         allCandidates[cNum].symbol = symbols[num - 1];
-        printf("Enter name of candidate %d: ", cNum + 1);
+        printf("Name of candidate %d: ", cNum + 1);
         scanf("%s", allCandidates[cNum].name);
         allCandidates[cNum].votes = 0;
     }
 }
 
+// show all candidates
 void displayAllCandidates()
 {
     for (int j = 0; j < candidateCount; j++) {
@@ -50,6 +100,7 @@ void displayAllCandidates()
     printf("\n");
 }
 
+// voting
 void getVotes(int voterCount)
 {
     displayAllCandidates();
@@ -65,34 +116,12 @@ void getVotes(int voterCount)
     }
 }
 
-int main()
-{
-    for (int i = 0; i < 11; i++) symbolTaken[i] = 0;
-
-    printf("Enter number of candidates: ");
-    scanf("%d", &candidateCount);
-    if (candidateCount >= MAX_C) {
-        printf("Too many candidates, bye\n");
-        return 0;
-    }
-
-    for (int i = 0; i < candidateCount; i++) fillCandidate(i);
-
-    int numVoters;
-    printf("Enter number of voters: ");
-    scanf("%d", &numVoters);
-
-    for (int i = 0; i < numVoters; i++) getVotes(i);
-
-    return 0;
-}
-
+// results
 void getResults()
 {
     int maxVotes = 0;
     int winnerIndex = -1;
     int winnerFrequency = 0;
-
     for (int i = 0; i < candidateCount; i++) {
         if (allCandidates[i].votes > maxVotes) {
             maxVotes = allCandidates[i].votes;
@@ -101,41 +130,20 @@ void getResults()
     }
 
     for (int i = 0; i < candidateCount; i++) {
-        if (allCandidates[i].votes == maxVotes) winnerFrequency++;
+        if (allCandidates[i].votes == maxVotes) {
+            winnerFrequency++;
+        }
     }
 
     printf("\nRESULTS\n");
 
-    if (winnerFrequency > 1) printf("No clear winner\n");
-    else if (winnerIndex != -1)
+    if (winnerFrequency > 1) {
+        printf("No clear winner\n");
+    } else if (winnerIndex != -1) {
         printf("Winner: %s (%c) with %d votes\n",
                allCandidates[winnerIndex].name,
-               allCandidates[winnerIndex].symbol,
-               maxVotes);
-    else printf("No winner\n");
-}
-
-// call getResults at end of main
-int main()
-{
-    for (int i = 0; i < 11; i++) symbolTaken[i] = 0;
-
-    printf("Enter number of candidates: ");
-    scanf("%d", &candidateCount);
-    if (candidateCount >= MAX_C) {
-        printf("Too many candidates, bye\n");
-        return 0;
+               allCandidates[winnerIndex].symbol, maxVotes);
+    } else {
+        printf("No winner\n");
     }
-
-    for (int i = 0; i < candidateCount; i++) fillCandidate(i);
-
-    int numVoters;
-    printf("Enter number of voters: ");
-    scanf("%d", &numVoters);
-
-    for (int i = 0; i < numVoters; i++) getVotes(i);
-
-    getResults();
-
-    return 0;
 }
